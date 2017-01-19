32b60954538f0218d466541b45927e6c778fee1c
Dylan Cochran <dylan.cochran@onyxpoint.com>
2017-01-19 14:23:41 -0500

    Fix debug output

--- lib/puppet/type/groupmembership.rb
+++ lib/puppet/type/groupmembership.rb
@@ -19,7 +19,7 @@ Puppet::Type.newtype(:groupmembership) do
   newproperty(:members, :array_matching => :all) do
     def insync?(is)
 	    Puppet.debug("XXX: is is a " + is.class.to_s + " with: " + is.to_s);
-	    Puppet.debug("XXX: @should is a " + @should.class.to_s + " with: " + is.to_s);
+	    Puppet.debug("XXX: @should is a " + @should.class.to_s + " with: " + @should.to_s);
 	    Puppet.debug("XXX: result should be:" + (@should.sort == is.sort).to_s);
       if is.is_a?(Array) and @should.is_a?(Array)
         is.sort == @should.sort
